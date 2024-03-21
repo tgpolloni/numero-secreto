7788b86e75b5c3e757cca6c174b14c0939c47b94
TIAGO <tgpolloni@gmail.com>
2024-03-21 23:25:50 +0100

    Projeto finalizado

--- app/validacao.js
+++ app/validacao.js
@@ -3,16 +3,28 @@ function verificaSeOChutePossueUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor Invalido!</div>'
+        return
     }
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `
         <div>Valor invalido: o numero secreto precisa estar entre ${menorValor} e ${maiorValor}</div>
         `
+        return
     }
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Voce acertou!!!</h2>
-            <h3>O numero secreto era: ${numeroSecreto}</h3>
+            <h3>O numero secreto é: ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O numero secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O numero secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
 }
@@ -24,3 +36,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido (numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
